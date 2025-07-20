2b0bc32fad36abb2cfa71b0fa622d30758ce245e
Tim Keller <tkeller@bridge12.com>
2025-07-19 21:55:50 -0400

    Autodetecting serial port

--- Software/Python/pypulsegen/examples/compile_pulse_program.py
+++ Software/Python/pypulsegen/examples/compile_pulse_program.py
@@ -1,9 +1,11 @@
 import pypulsegen as pg
+import time
 
+start_time = time.time()
 pulse_program = '''
 delay 1000e-9
 pulse 40e-9
-delay 600e-9
+delay 200e-9
 pulse 80e-9
 '''
 
@@ -15,18 +17,23 @@ config.start_addr = 1
 #print('-'*50)
 #print(config)
 
-commands = pg.core.parse_pulse_program(pulse_program)
-master_edges = pg.core.locate_master_edges(commands)
-edges = pg.core.locate_edges(master_edges, config)
-updated_edges = pg.core.merge_edges_connectivity(edges, config)
-sorted_edges = pg.core.sort_edges(updated_edges)
-all_states = pg.core.compile_states(sorted_edges, config)
-instructions = pg.core.generate_instructions(all_states, config)
-inst_bytes = pg.core.instructions_to_bytes(instructions)
-
-pg.plot_pulse_sequence(instructions, config)
+#commands = pg.core.parse_pulse_program(pulse_program)
+#master_edges = pg.core.locate_master_edges(commands)
+#edges = pg.core.locate_edges(master_edges, config)
+#updated_edges = pg.core.merge_edges_connectivity(edges, config)
+#sorted_edges = pg.core.sort_edges(updated_edges)
+#all_states = pg.core.compile_states(sorted_edges, config)
+#instructions = pg.core.generate_instructions(all_states, config)
+#inst_bytes = pg.core.instructions_to_bytes(instructions)
+#
+#pg.plot_pulse_sequence(instructions, config)
 
 inst_bytes = pg.compile_pulse_program(pulse_program, config)
+compile_time = time.time()
+print(f'Compile Time: {(compile_time - start_time)*1e6} us')
+print(f'Compile Time: {(compile_time - start_time)*1e9} ns')
+print(compile_time - start_time)
+print(compile_time, start_time)
 #inst_bytes = pg.core.compile_pulse_program(pulse_program, config)
 
 #print(inst_bytes)
@@ -42,4 +49,6 @@ for inst in inst_bytes:
 pg.stop()
 pg.upload_sequence(inst_bytes)
 pg.start()
+end_time = time.time()
 
+print(end_time - start_time)
