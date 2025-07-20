e3885064a0ad98409880fdedd01d786a8a86dc8b
Tim Keller <tkeller@bridge12.com>
2025-07-20 10:42:23 -0400

    Cleaning up example script

--- Software/Python/pypulsegen/examples/compile_pulse_program.py
+++ Software/Python/pypulsegen/examples/compile_pulse_program.py
@@ -1,43 +1,16 @@
 import pypulsegen as pg
-import time
 
-start_time = time.time()
 pulse_program = '''
 delay 1000e-9
 pulse 40e-9
-delay 200e-9
+delay 500e-9
 pulse 80e-9
 '''
 
 config = pg.load_config_from_json('config.json')
 config.rep_time = 0.1
-config.start_addr = 1
-#print('-'*50)
-#print('CONFIG')
-#print('-'*50)
-#print(config)
-
-#commands = pg.core.parse_pulse_program(pulse_program)
-#master_edges = pg.core.locate_master_edges(commands)
-#edges = pg.core.locate_edges(master_edges, config)
-#updated_edges = pg.core.merge_edges_connectivity(edges, config)
-#sorted_edges = pg.core.sort_edges(updated_edges)
-#all_states = pg.core.compile_states(sorted_edges, config)
-#instructions = pg.core.generate_instructions(all_states, config)
-#inst_bytes = pg.core.instructions_to_bytes(instructions)
-#
-#pg.plot_pulse_sequence(instructions, config)
 
 inst_bytes = pg.compile_pulse_program(pulse_program, config)
-compile_time = time.time()
-print(f'Compile Time: {(compile_time - start_time)*1e6} us')
-print(f'Compile Time: {(compile_time - start_time)*1e9} ns')
-print(compile_time - start_time)
-print(compile_time, start_time)
-#inst_bytes = pg.core.compile_pulse_program(pulse_program, config)
-
-#print(inst_bytes)
-
 
 print('-'*50)
 print('INSTRUCTION BYTES')
@@ -49,6 +22,3 @@ for inst in inst_bytes:
 pg.stop()
 pg.upload_sequence(inst_bytes)
 pg.start()
-end_time = time.time()
-
-print(end_time - start_time)
